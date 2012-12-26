6dd545056d761cbc6f0dac4dd547c7925d1aded6
wadmiraal <wad@wouteradmiraal.com>
2012-12-26 09:30:57 +0100

    Make SASS compile to local CSS directory, so Jekyll doesn't overwrite it. Ignore compiled CSS file.

--- src/_sass/config.rb
+++ src/_sass/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/../_site/css"
+css_dir = "/../css"
 sass_dir = "/"
-images_dir = "/../_site/img"
-javascripts_dir = "/../_site/js"
+images_dir = "/../img"
+javascripts_dir = "/../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
