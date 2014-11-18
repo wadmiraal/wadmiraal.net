204a2e6cca3dca822cfb17ef33a0d0eb67dd3d61
Wouter Admiraal <wad@wadmiraal.net>
2014-11-18 20:15:49 +0100

    Update gitignore. Add new Gulp task for minifying JS. Add CSS vendor and JS stub files.

---
+++ js/blog.fx.js
@@ -0,0 +1,8 @@
+/**
+ * @file
+ * Blog effects.
+ *
+ * This file defines the different UI effects, like opening the navigation
+ * or activating the JS enhanced layout.
+ */
+
---
+++ js/blog.pushstate.js
@@ -0,0 +1,7 @@
+/**
+ * @file
+ * This file activates the AJAX navigation.
+ *
+ * The blog is can load pages and posts via AJAX for faster navigation on the
+ * site. It uses pushState to update the page navigation.
+ */
