757334c0dfb125e1fdf67c2da2a041e056441c2c
Wouter Admiraal <wouter.admiraal@educa.ch>
2014-11-18 17:09:58 +0100

    Add Gulp Compass task.

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,21 @@
  * @link http://gulpjs.com/ See GulpJS for more information @endlink.
  */
 
-var gulp = require('gulp');
+var gulp = require( 'gulp' );
+var compass = require( 'gulp-compass' );
+var jekyll = require( 'gulp-jekyll' );
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Compile the SCSS files using Compass.
+gulp.task( 'compass', function() {
+  gulp.src( './sass/*.scss' )
+    .pipe( compass({
+      style: 'compressed',
+      sass: 'sass',
+      css: 'jekyll-src/css',
+      image: 'jekyll-src/img'
+    }))
+    .pipe( gulp.dest( 'tmp' ) );
 });
 
+// Default tasks.
+gulp.task( 'default', [ 'compass' ]);
