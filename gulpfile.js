c410783d94e64b1a44c8df5fdfaccd2d1c2d288c
Wouter Admiraal <wouter.admiraal@educa.ch>
2014-11-18 16:50:23 +0100

    Add Gulp integration.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Gulp task file.
+ *
+ * @link http://gulpjs.com/ See GulpJS for more information @endlink.
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
